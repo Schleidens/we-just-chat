982ee3e07016f2c85305ba57188b1ac217cf5592
Schleidens <alectineschleidens2016@gmail.com>
2024-01-19 22:55:15 -0500

    install firebase and make connection with google

---
+++ frontend/src/firebase/auth.ts
@@ -0,0 +1,16 @@
+import { auth } from './main';
+import {
+  GoogleAuthProvider,
+  UserCredential,
+  signInWithPopup,
+  signOut,
+} from 'firebase/auth';
+
+export const handleSignInWithGoogle = async (): Promise<UserCredential> => {
+  const googleAuthProvider = new GoogleAuthProvider();
+  return await signInWithPopup(auth, googleAuthProvider);
+};
+
+export const handleSignOut = async (): Promise<void> => {
+  return await signOut(auth);
+};
--- frontend/src/pages/Login/index.tsx
+++ frontend/src/pages/Login/index.tsx
@@ -1,13 +1,58 @@
 import React from 'react';
+import { useState } from 'react';
+
+import { handleSignInWithGoogle } from '../../firebase/auth';
 
 import './style.scss';
 
 const LoginPage = () => {
+  const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [isDisabled, setIsDisabled] = useState<boolean>(false);
+
+  const handleSignIn = async (): Promise<void> => {
+    try {
+      setIsDisabled(true);
+      await handleSignInWithGoogle();
+    } catch (error) {
+      if (error instanceof Error) {
+        setErrorMessage(error.message);
+      } else {
+        setErrorMessage("Something's wrong try again :)");
+      }
+
+      setTimeout(() => {
+        setErrorMessage(null);
+      }, 5000);
+    } finally {
+      setIsDisabled(false);
+    }
+  };
+
   return (
     <>
       <main className='login__page'>
+        <div className='intro'>
+          <p>I just chat!</p>
+          <p>You just chat!</p>
+          <p>
+            So ?? <span>We Just Chat :)</span>
+          </p>
+        </div>
         <div className='login'>
-          <button className='btn btn-dark'>Login with google</button>
+          <div
+            onClick={handleSignIn}
+            className='btn btn-dark'
+            style={isDisabled ? { opacity: '0.5', pointerEvents: 'none' } : {}}
+          >
+            {!isDisabled && (
+              <div>
+                {' '}
+                <div>SignIn with Google</div>
+              </div>
+            )}
+            {isDisabled && <div>SignIn.....</div>}
+          </div>
+          {errorMessage && <div className='error'>{errorMessage}</div>}
         </div>
       </main>
     </>
